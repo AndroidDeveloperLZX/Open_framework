ed810077a41a461c70322440c8e6e4685fb3d4a3
AndroidDeveloperLZX <936951222@qq.com>
2016-06-03 17:17:04 +0800

    测试GitHub

--- app/src/main/java/com/qianfeng/temp/MainActivity.java
+++ app/src/main/java/com/qianfeng/temp/MainActivity.java
@@ -2,12 +2,18 @@ package com.qianfeng.temp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //测试GitHub提交代码
+        textView = (TextView) findViewById(R.id.tv);
+        textView.setText("Hello GitHub");
     }
 }
